5c4a5b31764407e067615a301a7e61a61ee1fcc7
jinyounglee <hs25823@naver.com>
2021-04-07 02:52:39 +0900

    vim test

---
+++ 01.Hash/02.전화번호목록/Phonebook.py
@@ -0,0 +1,12 @@
+def solution(phone_book):
+    hash_table = {}
+    for number in phone_book:
+       hash_table[number] = 1
+    
+
+
+print(solution(["119","245","11922"]))
+print(solution(["4732","245","11922"]))
+
+
+
